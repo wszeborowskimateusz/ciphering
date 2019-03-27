05adbb64f0a35c43f323c02b2ad2add07fcebf1f
Mateusz <wszeborowskimateusz@gmail.com>
2019-03-27 18:32:15 +0100

    Adding RSA classes

---
+++ Client/Client/RSADecryptor.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Client
+{
+    class RSADecryptor
+    {
+        private RSACryptoServiceProvider csp;
+
+        public RSADecryptor(string privateKeyFileName)
+        {
+            csp = new RSACryptoServiceProvider();
+
+            XmlDocument doc = new XmlDocument();
+            doc.Load(privateKeyFileName);
+            string xmlcontents = doc.InnerXml;
+            csp.FromXmlString(xmlcontents);
+            var privKey = csp.ExportParameters(true);
+
+
+            csp.ImportParameters(privKey);
+        }
+
+        public string Decrypt(string message)
+        {
+            var bytesCypherText = Convert.FromBase64String(message);
+            string plainTextData = "";
+            try
+            {
+                var bytesPlainTextData = csp.Decrypt(bytesCypherText, false);
+                plainTextData = System.Text.Encoding.Unicode.GetString(bytesPlainTextData);
+            }
+            catch (Exception e)
+            {
+                Console.Out.WriteLine(e.Message);
+            }
+
+            return plainTextData;
+        }
+    }
+}
---
+++ Server/Server/RSAEncryptor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Server
+{
+    class RSAEncryptor
+    {
+        private RSACryptoServiceProvider csp;
+
+        public RSAEncryptor(string publicKeyFileName)
+        {
+            csp = new RSACryptoServiceProvider();
+
+            XmlDocument doc = new XmlDocument();
+            doc.Load(publicKeyFileName);
+            string xmlcontents = doc.InnerXml;
+            csp.FromXmlString(xmlcontents);
+            var pubKey = csp.ExportParameters(false);
+
+            csp = new RSACryptoServiceProvider();
+            csp.ImportParameters(pubKey);
+        }
+
+        public string Encrypt(string messaege)
+        {
+            var bytesPlainTextData = System.Text.Encoding.Unicode.GetBytes(messaege);
+
+            var bytesCypherText = csp.Encrypt(bytesPlainTextData, false);
+
+            var cypherText = Convert.ToBase64String(bytesCypherText);
+
+            return cypherText;
+        }
+    }
+}
