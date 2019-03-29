ecfcf0373094a45f09e7cdf587f74406ec3c334b
Mateusz <wszeborowskimateusz@gmail.com>
2019-03-29 19:54:07 +0100

    Adding receving file on a client side

--- Client/Client/TCPServer.cs
+++ Client/Client/TCPServer.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Client
 {
@@ -12,6 +13,7 @@ namespace Client
     {
         private Int32 ServerPort = 13529;
         private TcpListener server;
+        private string outputFileLocation = "C:\\Users\\Mateusz\\Desktop\\BSK_projekty\\Ciphering\\OutputFiles";
 
         public TCPServer()
         {
@@ -34,9 +36,33 @@ namespace Client
             }
         }
 
-        private void ProcessClient(NetworkStream networkStream)
+        private void ProcessClient(NetworkStream stream)
         {
-            
+            string fileName = "";
+            string fileHeader = "";
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                fileName = reader.ReadLine();
+                fileHeader = reader.ReadLine();
+            }
+
+            fileName = outputFileLocation + "\\" + fileName;
+
+            using (StreamWriter sw = File.CreateText(fileName))
+            {
+                sw.WriteLine(fileHeader);
+            }
+
+            using (BinaryWriter writer = new BinaryWriter(File.Open(fileName, FileMode.Open)))
+            {
+                int numBytesRead;
+                while ((numBytesRead = stream.Read(data, 0, data.Length)) > 0)
+                {
+                    writer.Write(data, 0, numBytesRead);
+                }               
+            }
+
+            stream.Close();
         }
     }
 }
